41537590834f59f316f5e83035c2007b0cc0dce2
gibo3 <gibo3057@gmail.com>
2025-05-08 19:31:52 +0900

    Add files via upload

---
+++ rungekutta4.m
@@ -0,0 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Runge-Kutta method of order 4 %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% f     : function
+% x0    : initial value
+% tspan : time span
+% dt    : discrete time size
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [t, x] = rungekutta4(f, tspan, x0, dt)
+t = (tspan(1):dt:tspan(2))';
+x = zeros(size(t,1),size(x0,2));
+x(1,:) = x0;
+
+for l = 1:size(t,1)-1
+    k1 = f(1,x(l,:))*dt;
+    k2 = f(1,x(l,:)+k1'/2)*dt;
+    k3 = f(1,x(l,:)+k2'/2)*dt;
+    k4 = f(1,x(l,:)+k3')*dt;
+    
+    x(l+1,:) = x(l,:)+(k1+2*k2+2*k3+k4)'/6;
+end
+return;
